e7f0877b5892bf154190c055811a091e3afb9c56
unknown <prabink.creation@gmail.com>
2020-03-16 19:44:23 +0545

    Add Votes answer

--- app/Question.php
+++ app/Question.php
@@ -10,7 +10,7 @@ class Question extends Model
  protected $fillable = ['title','body'];
  
     public function user(){
-        return $this>belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     
@@ -18,6 +18,28 @@ class Question extends Model
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        if ($this->answers > 0) {
+            if ($this->best_answer_id) {
+                return "answered-accepted";
+            }
+            return "answered";
+        }
+        return "unanswered";
     }
 
 
